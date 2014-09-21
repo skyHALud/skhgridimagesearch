bc07e6b0b446fe6e83201f417c034253c0fb5363
renyedi <renyedi@ebay.com>
2014-09-20 21:51:33 -0700

    Search settings activity finished, added model for search settings

--- SKHGridImageSearch/src/com/codepath/skhgridimagesearch/activities/SearchFilterActivity.java
+++ SKHGridImageSearch/src/com/codepath/skhgridimagesearch/activities/SearchFilterActivity.java
@@ -32,7 +32,9 @@ public class SearchFilterActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_save_filter) {
+			
+			
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
---
+++ SKHGridImageSearch/src/com/codepath/skhgridimagesearch/models/SearchSettings.java
@@ -0,0 +1,12 @@
+package com.codepath.skhgridimagesearch.models;
+
+import java.io.Serializable;
+
+public class SearchSettings implements Serializable {
+	private static final long serialVersionUID = -6705302715964430211L;
+
+	public String imageSize;
+	public String imageColor;
+	public String imageType;
+	public String site;
+}
