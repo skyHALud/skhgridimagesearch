a578e6976c2ed3170875f71411dbf8ce9491afcd
renyedi <renyedi@ebay.com>
2014-09-24 13:58:42 -0700

    Advanced: User can share an image to their friends or email it to themselves

--- SKHGridImageSearch/src/com/codepath/skhgridimagesearch/activities/ImageDisplayActivity.java
+++ SKHGridImageSearch/src/com/codepath/skhgridimagesearch/activities/ImageDisplayActivity.java
@@ -1,9 +1,20 @@
 package com.codepath.skhgridimagesearch.activities;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.codepath.skhgridimagesearch.R;
@@ -12,15 +23,17 @@ import com.squareup.picasso.Picasso;
 
 public class ImageDisplayActivity extends Activity {
 
+	private ImageView ivImageResult;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_image_display);
 
-		getActionBar().hide();
+//		getActionBar().hide();
 		
 		ImageResult result = (ImageResult) getIntent().getSerializableExtra("result");
-		ImageView ivImageResult = (ImageView) findViewById(R.id.ivImageResult);
+		ivImageResult = (ImageView) findViewById(R.id.ivImageResult);
 		
 		Picasso.with(this).load(result.fullUrl).into(ivImageResult);
 	}
@@ -38,9 +51,53 @@ public class ImageDisplayActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_share_image) {
+			shareItem();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	// Can be triggered by a view event such as a button press
+	public void shareItem() {
+	    // Get access to the URI for the bitmap
+	    Uri bmpUri = getLocalBitmapUri(ivImageResult);
+	    if (bmpUri != null) {
+	        // Construct a ShareIntent with link to image
+	        Intent shareIntent = new Intent();
+	        shareIntent.setAction(Intent.ACTION_SEND);
+	        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+	        shareIntent.setType("image/*");
+	        // Launch sharing dialog for image
+	        startActivity(Intent.createChooser(shareIntent, "Share Image"));
+	    } else {
+	        // ...sharing failed, handle error
+	    }
+	}
+
+	// Returns the URI path to the Bitmap displayed in specified ImageView
+	public Uri getLocalBitmapUri(ImageView imageView) {
+	    // Extract Bitmap from ImageView drawable
+	    Drawable drawable = imageView.getDrawable();
+	    Bitmap bmp = null;
+	    if (drawable instanceof BitmapDrawable){
+	       bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+	    } else {
+	       return null;
+	    }
+	    // Store image to default external storage directory
+	    Uri bmpUri = null;
+	    try {
+	        File file =  new File(Environment.getExternalStoragePublicDirectory(  
+		        Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+	        file.getParentFile().mkdirs();
+	        FileOutputStream out = new FileOutputStream(file);
+	        bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+	        out.close();
+	        bmpUri = Uri.fromFile(file);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    return bmpUri;
 	}
 }
